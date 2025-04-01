950182573bad2b6abc3959950dcbd4bc6ccdf1aa
Eric Schmidt <eric0802@web.de>
2025-04-01 14:10:04 +0200

    add Motorcycle object and get its information

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,11 @@ public class Main {
 
         Vehicle vehicle = new Vehicle("Honda", "Civic", 2022);
         Car car = new Car("Honda", "Civic", 2022, 4);
+
         System.out.println(vehicle.getInformation());
         System.out.println(car.getInformation());
+
+        Motorcycle motorcycle = new Motorcycle("Honda", "CB125R", 2025, "Naked Bike");
+        System.out.println(motorcycle.getInformation());
     }
 }
